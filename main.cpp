5ac05e894168e7a349b9f429891a9c754f1f3482
ovictoraurelio <ovictoraurelio@gmail.com>
2016-10-31 00:05:12 -0300

    With a bug in file output

--- main.cpp
+++ main.cpp
@@ -4,28 +4,61 @@
 	* @since 17/08/16
 	*
 */
-#include <bits/stdc++.h>
-#define MAX_EXPR 200
+/*#include <bits/stdc++.h>*/
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+
+#include <algorithm>
+#include <fstream>
+
+#define MAX_SUB_EXPR 9999999
 
 using namespace std;
 
-string header, dashed;
-string exprGlobal;
-int sizeOfAllExprs;
+// header is a string to show in header of table
+// dashed is ------ of table
+// expr is a string scanned from keyboard or from file
+string header, dashed, expr;
+// a pair if index of start and ends of a expr in string
 vector<pair<int,int> > exprs;
-map<string, bool> resultsLine;
-// 		 x,y,z,t 		 x,y,z,t
+// results of each subexpr
+bool results[MAX_SUB_EXPR];
+// counter to trues and counter to falses at expr
+int cT=0,cF=0;
+// 		    x,y,z,t    		  x,y,z,t
 int m[4] = {0,0,0,0}, v[4] = {0,0,0,0};
+
+//Retornar true se o primeiro for menor.
+//Isso tá gigante, porque fiz várias tentativas para descobrir qual os critérios para ordenar as subexpressões no header e não consegui acertar.
+bool firstLessThenNext(pair<int, int> i, pair<int, int> j){
+	vector<int> iL, jL;
+	if(i.second - i.first > j.second - j.first) return false;
+	if(i.second - i.first < j.second - j.first) return true;
+	for(int k=i.first,l=j.first; k<i.second; k++,l++){		
+		 if(expr.at(k) != '.' && expr.at(k) != '+' && expr.at(k) != '>')
+		 	iL.push_back(k);
+		 if(expr.at(l) != '.' && expr.at(l) != '+' && expr.at(l) != '>')
+		 	jL.push_back(l);	
+	}
+	for(int k=0; k<iL.size(); k++){
+		if(expr.at(iL[k]) > expr.at(jL[k])){
+			return false;
+		}
+	}
+	return true;
+}
 
 int szM(){
 	int t=0;	
 	for(int i=0; i<4; i++) t += m[i];
 	return t;
 }
-// This method compare a string thats start at index i and goes to index l
-bool compareStr(char *s, int la, int ra, int lb, int rb){
+bool compareStr(string s, int la, int ra, int lb, int rb){
 	if(ra-la != rb-lb) return false;
-	for(int i=la; i<=ra; i++) if(s[i] != s[(i-la)+lb]) return false;	
+	for(int i=la; i<=ra; i++) if(s.at(i) != s.at((i-la)+lb)) return false;	
 	return true;
 }
 char itl(int i){//Int To Letter
@@ -47,11 +80,11 @@ int lti(char i){//Letter T oInt
 	};
 }
 //set in a vector, wich vars i'm use
-void setVarsOnExprs(char *s){
+void setVarsOnExprs(string s){
 	m[0] = m[1] = m[2] = m[3] = 0;
-	for(int i=0; i<strlen(s); i++)
-		if(lti(s[i]) != -1){
-			m[lti(s[i])] = 1;
+	for(int i=0; i<s.size(); i++)
+		if(lti(s.at(i)) != -1){
+			m[lti(s.at(i))] = 1;
 		}		
 }
 //calc a value for a minimal logic expression
@@ -81,20 +114,20 @@ bool solveMin(string e){
 	}
 }
 //getting all exprs inside ( ) of a expression
-void getExprs(char *s, int start, int end){	
-	stack<int> l;//left
-	for(int i=0; i<=end; i++){
-		if(s[i] == '('){			
-			l.push(i);
-		}else if(s[i] == ')'){			
-			exprs.push_back(pair<int,int>(l.top(), i));
-			l.pop();
+void getExprs(string s){
+	vector<int> l;//left
+	for(int i=0; i<s.size(); i++){
+		if(s.at(i) == '('){			
+			l.push_back(i);
+		}else if(s.at(i) == ')'){			
+			exprs.push_back(pair<int,int>(l.back(), i));
+			l.pop_back();
 		}
 	}
 }
 
 //removing equals exprs of a expresion
-void redundancyRemove(char *s){	
+void redundancyRemove(string s){	
 	for(int i=exprs.size()-1; i>=0; i--){
 		for(int j=0; j<i; j++){
 			if(i!=j && compareStr(s, exprs[i].first,exprs[i].second,exprs[j].first,exprs[j].second)){				
@@ -105,58 +138,36 @@ void redundancyRemove(char *s){
 }
 
 void solveExprs(){
-	string lookingFor, current, original;
-	size_t fSubExpr;	
+	string current;	
 	for(int i=0; i<exprs.size(); i++){		
-		current = exprGlobal.substr(exprs[i].first, exprs[i].second - exprs[i].first + 1);
-		original = current;	
+		current = expr.substr(exprs[i].first, exprs[i].second - exprs[i].first + 1);		
 		if(current.size() <= 5){// (x+y) ou -x						
-			resultsLine[current] = solveMin(current.c_str());
+			results[i] = solveMin(current.c_str());
 		}else{
-			/*for(int j=0; j<exprs.size(); j++){*/
-				/*lookingFor = exprGlobal.substr(exprs[j].first, exprs[j].second - exprs[j].first + 1);				
-				if(j < i){
-					while(current.find(lookingFor) != string::npos){//buscando as subexpressões e substituindo pelos valores já conhecidos
-						current.replace(current.find(lookingFor), lookingFor.size(),1,resultsLine[lookingFor] ? '1' : '0');
-					}
-				}*/
-				/*cout << " valores: x: " << cs['x'] << " y:"  << cs['y'] << " z: " << cs['z'] << " t: " << cs['t'];*/
-				/*cout << endl << endl << current << endl;*/
-
-				int last;
-				for(int k=0; k<current.size(); k++){					
-					if(current.at(k) == '('){			
-						last = k;
-					}else if(current.at(k) == ')'){
-						/*printf("inside %d-%d\n", last, k);
-						cout << " vou resolver: " << current.substr(last, k-last+1) << endl;*/
-						current.replace(last, k - last +1,1, solveMin(current.substr(last, k-last + 1)) ? '1':'0');
-						/*cout << " new current: " << current << endl;*/
-						//exprs.push_back(pair<int,int>(l.top(), k));
-						last = 0;
-						k=-1;
-					}
+			int last;
+			for(int k=0; k<current.size(); k++){			
+				if(current.at(k) == '('){			
+					last = k;
+				}else if(current.at(k) == ')'){
+					current.replace(last, k - last +1,1, solveMin(current.substr(last, k-last + 1)) ? '1':'0');					
+					last = 0;
+					k=-1;
 				}
-
-
-				resultsLine[original] = solveMin(current);
-				/*cout << "Solving for " << original << " torned: " << current << " value " << resultsLine[original] << endl;*/
-			/*}	*/					
+			}
+			results[i] = solveMin(current);				
 		}		
 	}
 }
 
-void showResults(){
-	solveExprs();
-	//for (std::map<string,bool>::iterator it=resultsLine.begin(); it!=resultsLine.end(); ++it)
-	for(int i=0; i<exprs.size(); i++){
-		string current = exprGlobal.substr(exprs[i].first, exprs[i].second - exprs[i].first + 1);
-		for(int b=0; b<exprs[i].second - exprs[i].first; b++) printf(" ");
-		printf("%d|",resultsLine[current]);
+void showResults(){	
+	solveExprs();	
+	for(int i=0; i<exprs.size(); i++){		
+		for(int b=0; b<exprs[i].second - exprs[i].first; b++) cout << " ";		
+		cout << (results[i]?1:0) << "|";
 	}
-	printf("\n");
+	results[exprs.size()-1] ? cT++ : cF++;//just consider last column of table
+	cout << endl;
 }
-
 
 void setValuesFromLine(int ind[4]){
 	for(int i=0,j=0; i<4; i++){
@@ -175,7 +186,7 @@ void solveTable(){
 				for(ind[3]=0; ind[3]<(size>3?2:0); ind[3]++){
 						setValuesFromLine(ind);					
 						cout << dashed << endl;								
-						printf("|%d|%d|%d|%d|", v[0], v[1], v[2], v[3]);						
+						printf("|%d|%d|%d|%d|", v[0], v[1], v[2], v[3]);
 						showResults();					
 				}
 				if(size==3){
@@ -195,92 +206,58 @@ void solveTable(){
 		if(size==1){
 			setValuesFromLine(ind);			
 			cout << dashed << endl;
-			printf("|%d|", ind[0]%2);			
+			printf("|%d|", ind[0]%2);
+			if(expr.size()<=2){				
+				cT++;cF++;
+			}
 			showResults();
 		}
 	}
 }	
 
-
 int main(){	
 	int c=0,highlights[50];
-	char expr[MAX_EXPR],*f;
-	scanf("%d", &c);
-	for(int k=1; k<=c; k++){
-		printf("Tabela #%d\n", k);
-		cin >> expr;
-		exprGlobal = string(expr);
-		setVarsOnExprs(expr);	
+	char *f;
+	ifstream inFile ("EntradaTabela.out", ios::binary);
+	if(inFile.is_open()){
+		cin >> c;
+		for(int k=1; k<=c; k++){
+			printf("Tabela #%d\n", k);
+			cin >> expr;
 
-		getExprs(expr, 0, strlen(expr));
-		//ainda é necessário ordenar as expressões por tamanho e caso o tamanho seja igual ordem lexicográfica.
-		//quando mudar de map para vector 
-		redundancyRemove(expr);
-		
-		header = "|" + string() + (m[0]?"x|":"") + string() + (m[1]?"y|":"") + string() + (m[2]?"z|":"") + string() + (m[3]?"t|":"") + string();
-		for(int i=0; i<exprs.size(); i++){
-			header += string(expr).substr(exprs[i].first, exprs[i].second - exprs[i].first + 1);
-			header += "|";
-		}			
-		for(int i=0; i<header.size(); i++) dashed += "-";
-		cout << dashed << endl << header << endl;
+			setVarsOnExprs(expr);		
+			getExprs(expr);		
+			
+			redundancyRemove(expr);
+			sort(exprs.begin(), exprs.end(), firstLessThenNext);
+			
+			header = "|" + string() + (m[0]?"x|":"") + string() + (m[1]?"y|":"") + string() + (m[2]?"z|":"") + string() + (m[3]?"t|":"") + string();
+			for(int i=0; i<exprs.size(); i++){
+				header += expr.substr(exprs[i].first, exprs[i].second - exprs[i].first + 1);
+				header += "|";
+			}			
+			for(int i=0; i<header.size(); i++) dashed += "-";
+			cout << dashed << endl << header << endl;
 
-		solveTable();
+			solveTable();
 		
-		cout << dashed << endl << endl;
-		exprs.clear();
-		dashed.clear();
-	}
+			cout << dashed << endl;		
+
+			if(cT > 0 && cF > 0 ){
+				cout << "satisfativel e refutavel";
+			}else if(cT > 0 and cF == 0){
+				cout << "satisfativel e tautologia";
+			}else{//if(cT == 0 && cF > 0)
+				cout << "insatisfativel e refutavel";
+			}
+			cout << endl << endl;
+
+			exprs.clear();
+			dashed.clear();
+			cT = cF = 0;
+			inFile.close();
+		}
+	}else 
+		cout << "Nao consegui abrir o arquivo";
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*void solveInputs(map<char,int> m, char *expr){
-	string s(expr);
-	vector<char> copia(s.begin(), s.end());
-	bool result = false;
-	int start=0, first=1, second=2, third=3;
-
-	int sAll = strlen(s);
-	stack<int> l;//left (
-
-	for(int i=0; i<=end; i++){
-		if(s[i] == '('){
-			l.push(i);
-		}else if(s[i] == ')'){		
-			start = i;
-			first = i+1;
-			second = i+2;
-			third = i+3;
-			while(copia[start] == '('){
-				if(copia[second] != '-' && copia[third] != '('){
-					result = calcExpr(m[copia[first]], m[copia[third]], copia[second]);				
-					copia.erase(v.begin() + start, v.begin() + second );//(-x) apago até o indice 2, no terceiro indce(4 caracter) fica o resultado..
-					copia[start] = result;
-				}else if(copia[first] == '-' && copia[first] != '('){
-					result = calcExpr(m[copia[second]], 0,'-');
-					copia.erase(v.begin() + start, v.begin() + third );//(x+y) mesma lógica.. só que aqui tem um caracter a mais...
-					copia[start] = result;
-				}
-			}
-		}
-	}
-	return sAll;
-
-	for(int i=0; i<exprs.size(); i++){
-		copia = vector<char>(s.begin(), s.end());
-
-		
-	}
-}**/
